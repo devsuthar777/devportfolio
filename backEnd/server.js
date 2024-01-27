1689883b9437c3a566ad5212eccde5275303f2b3
devsuthar777 <devsuthar777@gmail.com>
2024-01-28 01:56:20 +0530

    origin issue solution1

--- backEnd/server.js
+++ backEnd/server.js
@@ -29,11 +29,12 @@ app.listen(PORT, () =>{
 const dbConnect = require('./config/database');
 dbConnect();
 
-app.get('/',(req,res) => {
-    return res.json({
-        success:true,
-        message:'Your server is up and running..'
-    })
-});
+router.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+     });
 
 
