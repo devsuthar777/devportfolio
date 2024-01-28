f41e295d65b091d4a1fa84f5f92ea3d31b1ee394
devsuthar777 <devsuthar777@gmail.com>
2024-01-28 12:51:31 +0530

    git ignore .env

--- backEnd/server.js
+++ backEnd/server.js
@@ -29,12 +29,11 @@ app.listen(PORT, () =>{
 const dbConnect = require('./config/database');
 dbConnect();
 
-router.get("/", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Max-Age", "1800");
-    res.setHeader("Access-Control-Allow-Headers", "content-type");
-    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
-     });
+app.get('/',(req,res) => {
+    return res.json({
+        success:true,
+        message:'Your server is up and running..'
+    })
+});
 
 
