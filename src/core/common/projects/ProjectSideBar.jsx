71c052e931df5afe9a7066bd79d70473799eac1c
devsuthar777 <devsuthar777@gmail.com>
2024-02-08 00:04:17 +0530

    sidebar project dark mode done

--- src/core/common/projects/ProjectSideBar.jsx
+++ src/core/common/projects/ProjectSideBar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link, NavLink, useLocation } from 'react-router-dom'
 import { projectDetails } from '../../../data/projectDetails';
+import {AppContext} from '../../../context/AppContext';
 const ProjectSideBar = () => {
   const currLocation = useLocation();
   const currProject = currLocation.pathname.split(':').at(-1);
+  const {mode} = useContext(AppContext);
   //alert(currProject);
   
   return (
@@ -12,9 +14,9 @@ const ProjectSideBar = () => {
         {
             projectDetails.map((indexProject,index) => (
               
-              <Link  to={indexProject.path}><div className={` ${currProject==indexProject.projectName ? 'bg-blue-5 font-extrabold' : 'hover:bg-blue-25'} rounded-sm cursor-pointer  flex flex-row items-center py-[2px] text-md font-bold ${index==0 ? 'mt-[4px]' : ''  }
-              h-full w-full text-lg  `} key={index}><span className={`w-1    rounded-t-lg rounded-b-lg ${currProject==indexProject.projectName ? 'bg-black divCutAnimation' : 'bg-transparent'} 
-              mr-2 text-center `}></span><span>{indexProject.ProjectTitle}</span></div></Link>
+              <Link  to={indexProject.path}><div className={` ${currProject==indexProject.projectName ? (mode=='dark' ? 'bg-richblack-300 font-extrabold' : 'bg-blue-5 font-extrabold') : (mode=='dark' ? 'hover:bg-richblack-100' : 'hover:bg-blue-25')} rounded-sm cursor-pointer  flex flex-row items-center py-[2px] text-md font-bold ${index==0 ? 'mt-[4px]' : ''  }
+              h-full w-full text-lg  `} key={index}><span className={`w-1    rounded-t-lg rounded-b-lg ${currProject==indexProject.projectName ? (mode=='light' ? 'bg-black divCutAnimation' : 'bg-white divCutAnimation') : 'bg-transparent'} 
+              mr-2 text-center `}></span><span className={`${mode== 'dark' ? 'text-white' : ''}`}>{indexProject.ProjectTitle}</span></div></Link>
             )
             
             )
--- src/core/pages/ProjectsPage.jsx
+++ src/core/pages/ProjectsPage.jsx
@@ -1,17 +1,19 @@
 
-import React from 'react'
+import React, { useContext } from 'react'
 import ProjectSideBar from '../common/projects/ProjectSideBar'
 import { useLocation } from 'react-router-dom'
+import { AppContext } from '../../context/AppContext'
 
 const ProjectsPage = () => {
     const currLocation = useLocation();
+    const {mode} = useContext(AppContext);
     //alert(currLocation.pathname);
     
 
   return (
     <div className='mt-16 '>     
         <div className='w-full flex flex-row '>
-            <div className='w-[15%] border  fixed'><ProjectSideBar/></div>
+            <div className={`${mode == 'dark' ? 'w-[15%] border-r border-white fixed' : 'w-[15%] border-r fixed'}`}><ProjectSideBar/></div>
             <div className=' h-full  w-[85%] ml-[15%]'>
                 
             </div>
