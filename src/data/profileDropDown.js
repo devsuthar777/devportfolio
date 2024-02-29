b41490e3c3ec95edf4a0b74b3c370e0495c0107e
devsuthar777 <devsuthar777@gmail.com>
2024-02-29 23:48:45 +0530

    profile pic issue solved

--- src/data/profileDropDown.js
+++ src/data/profileDropDown.js
@@ -1,3 +1,4 @@
+import clogo from "../core/assests/png/1703787378825.jpg";
 export const profileDropLogo = {
-    link:"https://media.licdn.com/dms/image/D4D03AQG1e8QJmtVN3A/profile-displayphoto-shrink_800_800/0/1703787378825?e=1709164800&v=beta&t=cNUaNsFPzy2ScYBUrx_px3m4QZTtdlFm9oLNHp1Lfa0"
+    link:clogo
 };
